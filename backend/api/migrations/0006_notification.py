c2a2b6accd02e950a37dcf2232d33c7615029146
M10D12 <107938456+M10D12@users.noreply.github.com>
2025-12-28 22:03:41 +0000

    alertas fix

--- backend/api/migrations/0006_notification.py
+++ backend/api/migrations/0006_notification.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0001_initial'),
+        ('api', '0004_group_name_unique'),
     ]
 
     operations = [
